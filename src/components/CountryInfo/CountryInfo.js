cf1363a34f1cc5a576b9ef81431d7dba54fe7d84
Nicolas Calvello <nic.calv@outlook.it>
2021-10-30 15:47:58 +0200

    add CountryInfo which for country and flag

---
+++ src/components/CountryInfo/CountryInfo.js
@@ -0,0 +1,19 @@
+import countriesMap from "./countries-map"
+
+function CountryInfo({ countryName }) {
+  const countryCode = countriesMap[countryName]
+  return (
+    <div className="flex flex-col items-center border-gray-700 pt-4 mr-4">
+      <img
+          className="rounded border border-black"
+          src={`https://flagcdn.com/w40/${countryCode}.png`}
+          srcSet={`https://flagcdn.com/w80/${countryCode}.png 2x`}
+          width="40"
+          alt={countryName} 
+      />
+      <span className="block mt-1 text-xs text-gray-500">{countryName}</span>
+    </div>
+  )
+}
+
+export default CountryInfo
