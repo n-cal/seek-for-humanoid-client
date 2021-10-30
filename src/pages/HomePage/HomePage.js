afee787a6b5e41d69205945c421e93afd4b36584
Nicolas Calvello <nic.calv@outlook.it>
2021-10-30 15:50:25 +0200

    add HomePage for humanoids list

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,112 @@
+import { useEffect, useState } from "react";
+import SearchController from "../../components/SearchController/SearchController";
+import HumanoidsList from "../../components/HumanoidsList/HumanoidsList";
+import PageSelector from "../../components/PageSelector/PageSelector";
+
+function HomePage(props) {
+  const [humanoids, setHumanoids] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pagesCount, setPagesCount] = useState(0);
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [searchName, setSearchName] = useState("");
+  const [queryParams, setQueryParams] = useState("");
+  const [haveHumanoids, setHaveHumanoids] = useState(false);
+
+  useEffect(() => {
+    console.log(queryParams);
+    console.log(`pag=${currentPage}`);
+    fetch(
+      `http://127.0.0.1:8000/api/humanoids?pag=${currentPage}` +
+        (queryParams ? "&" + queryParams : "")
+    )
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error("error fetching humanoids");
+        } else {
+          return response.json();
+        }
+      })
+      .then((json) => {
+        if (json.count === 0) {
+          throw new Error("no humanoids in response");
+        }
+
+        setPagesCount(Math.floor(json.count / json.results.length));
+        setHumanoids(json.results);
+        setHaveHumanoids(true);
+      })
+      .catch((err) => {
+        setHaveHumanoids(false);
+        setHumanoids([]);
+      });
+  }, [currentPage, queryParams]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/countries")
+      .then((response) => response.json())
+      .then((json) => {
+        setCountries(json);
+        if (!json.includes(selectedCountry)) {
+          setSelectedCountry("");
+        }
+      })
+      .catch((err) => {
+        setCountries([]);
+      });
+  }, [currentPage, queryParams]);
+
+  function makeQuery() {
+    const params = new URLSearchParams();
+
+    if (selectedCountry) {
+      params.append("country", selectedCountry);
+    }
+
+    let searchValue = searchName
+      .split(" ")
+      .filter((ch) => ch)
+      .join(" ");
+
+    if (searchValue) {
+      params.append("search", searchValue);
+    }
+
+    if (!params.has("country") && !params.has("search")) {
+      setCurrentPage(1);
+      setQueryParams("");
+      return;
+    }
+
+    setCurrentPage(1);
+    setQueryParams(params.toString());
+  }
+
+  return (
+    <div className="flex flex-col items-center p-3 text-gray-600 sm:flex-row-reverse sm:justify-center sm:items-start">
+      <SearchController
+        makeQuery={makeQuery}
+        setSearchName={setSearchName}
+        setSelectedCountry={setSelectedCountry}
+        countries={countries}
+      />
+      <div className="flex-col items-stretch flex-grow max-w-xl">
+        {haveHumanoids ? (
+          <HumanoidsList humanoids={humanoids} />
+        ) : (
+          <h2 className="text-center text-xl">No Humanoids Found</h2>
+        )}
+        {/* <PageSelector pagesCount={4} currentPage={4} setCurrentPage={setCurrentPage} /> */}
+        {haveHumanoids && (
+          <PageSelector
+            pagesCount={pagesCount}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
