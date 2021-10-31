84fa9307daacbf309c7204958b89aba20d26f12a
Nicolas Calvello <nic.calv@outlook.it>
2021-10-31 11:49:14 +0100

    add Link to HumanoidItem

--- src/components/HumanoidItem/HumanoidItem.js
+++ src/components/HumanoidItem/HumanoidItem.js
@@ -1,21 +1,24 @@
+import { Link } from "react-router-dom";
 import CountryInfo from "../CountryInfo/CountryInfo";
 
 function HumanoidItem({ humanoid }) {
   return (
-    <div className="cursor-pointer bg-white shadow rounded flex items-center pl-4 py-3 mb-2 overflow-hidden sm:flex-row-reverse sm:justify-between sm:pr-4 sm:mr-5">
-      <img
-        className="rounded-full"
-        src={`http://${humanoid.thumbnail_url}`}
-        alt={humanoid.name}
-      />
+    <Link to={`/detail/${humanoid.id}`}>
+      <div className="cursor-pointer bg-white shadow rounded flex items-center pl-4 py-3 mb-2 overflow-hidden sm:flex-row-reverse sm:justify-between sm:pr-4 sm:mr-5">
+        <img
+          className="rounded-full"
+          src={`http://${humanoid.thumbnail_url}`}
+          alt={humanoid.name}
+        />
 
-      <div className="flex flex-col justify-between items-start p-2 sm:flex-row-reverse sm:flex-grow sm:max-w-xs sm:items-center">
-        <p className="block text-xl">
-          {humanoid.name} {humanoid.surname}
-        </p>
-        <CountryInfo countryName={humanoid.country} />
+        <div className="flex flex-col justify-between items-start p-2 sm:flex-row-reverse sm:flex-grow sm:max-w-xs sm:items-center">
+          <p className="block text-xl">
+            {humanoid.name} {humanoid.surname}
+          </p>
+          <CountryInfo countryName={humanoid.country} />
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
