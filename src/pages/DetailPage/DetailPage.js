c9a8d16cfd63c5bcc7a3975302114d21e7582711
Nicolas Calvello <nic.calv@outlook.it>
2021-11-01 22:02:05 +0100

    fix className props

--- src/pages/DetailPage/DetailPage.js
+++ src/pages/DetailPage/DetailPage.js
@@ -31,41 +31,42 @@ function DetailPage(props) {
 
   return (
     <div
-      class="
+      className="
       flex flex-col
       text-gray-600
       sm:flex-row sm:items-start sm:justify-center
       p-2
     "
     >
-      <div class="flex-col items-center mt-3 max-w-2xl sm:mr-3">
-        <div class="flex items-center justify-between bg-white shadow rounded p-3">
+      <div className="flex-col items-center mt-3 max-w-2xl sm:mr-3">
+        <div className="flex items-center justify-between bg-white shadow rounded p-3">
           <Link to="/">
-            <button class="text-xs bg-green-200 rounded p-1 shadow mr-3 hover:bg-green-50">
+            <button className="text-xs bg-green-200 rounded p-1 shadow mr-3 hover:bg-green-50">
               {"<-"} back to list
             </button>
           </Link>
-          <h1 class="text-3xl m-auto">
+          <h1 className="text-3xl m-auto">
             {humanoid.name} {humanoid.surname}
           </h1>
           <CountryInfo countryName={humanoid.country} />
         </div>
 
-        <div class="bg-white rounded p-4 shadow mt-3">
-          <h2 class="text-lg font-bold">{">"} Humanoid Bio</h2>
+        <div className="bg-white rounded p-4 shadow mt-3">
+          <h2 className="text-lg font-bold">{">"} Humanoid Bio</h2>
           <p>{humanoid.bio}</p>
         </div>
       </div>
 
-      <div class="flex flex-col">
-        <div class="flex justify-center mt-3 sm:min-w-max">
+      <div className="flex flex-col">
+        <div className="flex justify-center mt-3 sm:min-w-max">
           <img
-            class="bg-white p-3 rounded shadow"
+            className="bg-white p-3 rounded shadow"
             src={process.env.REACT_APP_API_URL + humanoid.img_url}
+            alt={humanoid.name}
           />
         </div>
-        <div class="bg-white rounded shadow p-3 mt-3">
-          <h2 class="text-lg font-bold">{">"} Details</h2>
+        <div className="bg-white rounded shadow p-3 mt-3">
+          <h2 className="text-lg font-bold">{">"} Details</h2>
           <p>email - {humanoid.email}</p>
           <p>address - {humanoid.address}</p>
           <p>city - {humanoid.city}</p>
